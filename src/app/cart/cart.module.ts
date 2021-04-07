822d7145e4b1dc68aa39c0b47097c04336c03e14
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-04-07 15:08:06 +0530

    cart route

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -4,6 +4,7 @@ import { CartComponent } from './components/cart/cart.component';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 import { CartListComponent } from './components/cart-list/cart-list.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { CartRoutingModule } from './cart.routing';
 
 
 
@@ -15,11 +16,12 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
     CheckoutComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CartRoutingModule
   ],
   exports: [
-    // temp fix until we have routing
-    CartComponent
+    // temp fix until we have routing, for routes, no need to export the components
+    // CartComponent
   ]
 })
 export class CartModule { }
---
+++ src/app/cart/cart.routing.ts
@@ -0,0 +1,35 @@
+// cart.routing.ts
+
+import {NgModule} from '@angular/core';
+
+import {RouterModule, Routes} from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+ 
+const routes: Routes = [
+    {
+        // hosted inside app.component.html
+        path: 'cart',
+        component: CartComponent,
+    },
+    {
+         // hosted inside app.component.html
+        path: 'checkout',
+        component: CheckoutComponent
+    }
+]
+
+@NgModule({
+    imports: [
+        // lazy loaded/sub module, forChild has fewer services
+        RouterModule.forChild(routes)
+    ],
+    exports: [
+        // component, directives, pipes
+        // Modules, all the component, directives, pipes exported in module are re-exported
+        RouterModule
+    ]
+})
+export class CartRoutingModule {
+
+}
